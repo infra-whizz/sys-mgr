efbd3a64676680d5ae2b6b64235c6d6b9c3d2086
Bo Maryniuk <bogdan.maryniuk@elektrobit.com>
2021-03-22 16:08:15 +0100

    Include /etc into src maker

--- pkg/mksrc.sh
+++ pkg/mksrc.sh
@@ -4,7 +4,7 @@
 P_NAME="sysroot-manager"
 P_APP="cmd/sys-mgr.go"
 P_SRC_DIRS=("arch" "pm" "sr" "cmd")
-P_DOC_DIRS=("doc")
+P_DOC_DIRS=("etc")
 P_FILES=("LICENSE" "README.md" "go.mod" "go.sum")
 P_CMD=("Makefile")
 
