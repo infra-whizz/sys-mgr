a21e7f036494462d2acb384b206080ae2480504c
Bo Maryniuk <bo@maryniuk.net>
2021-05-28 20:29:57 +0200

    Add methods to determine if it is chrooted environment or not

--- sr/mgr.go
+++ sr/mgr.go
@@ -3,6 +3,7 @@ package sysmgr_sr
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"path"
 	"strings"
 
@@ -10,7 +11,9 @@ import (
 	"github.com/thoas/go-funk"
 )
 
-var defaultSysrootPath string = "/usr/sysroots"
+var DefaultSysrootPath string = "/usr/sysroots"
+var HostSysrootConfig string = "/etc/sysroots.conf"
+var ChildSysrootConfig string = "/etc/sysroot.conf"
 
 type SysrootManager struct {
 	sysroots      string
@@ -21,7 +24,7 @@ func NewSysrootManager(conf *nanoconf.Config) *SysrootManager {
 	srm := new(SysrootManager)
 	srm.sysroots = conf.Root().String("sysroots", "")
 	if srm.sysroots == "" {
-		srm.sysroots = defaultSysrootPath
+		srm.sysroots = DefaultSysrootPath
 	}
 	srm.architectures = []string{}
 	return srm
@@ -123,18 +126,56 @@ func (srm *SysrootManager) GetSysRoots() ([]*SysRoot, error) {
 	return roots, nil
 }
 
-// GetDefaultSysroot
+// GetDefaultSysroot. If chrooted, returns current
 func (srm *SysrootManager) GetDefaultSysroot() (*SysRoot, error) {
-	sysroots, err := srm.GetSysRoots()
+	isChrooted, err := srm.IsChrooted()
+
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("Unable to determine the chroot environment: %s", err.Error())
 	}
 
-	for _, sr := range sysroots {
-		if sr.Default {
-			return sr, nil
+	if !isChrooted {
+		sysroots, err := srm.GetSysRoots()
+		if err != nil {
+			return nil, err
 		}
+
+		for _, sr := range sysroots {
+			if sr.Default {
+				return sr, nil
+			}
+		}
+	} else {
+		return NewSysRoot("/").Init()
 	}
 
 	return nil, fmt.Errorf("No default system root has been found. Please setup one.")
+}
+
+// fileExists or not. This needs to be moved to utils, but importing them causes cycle.
+// Utils needs to be moved into a separate sub-package.
+func (srm *SysrootManager) fileExists(filepath string) (bool, error) {
+	_, err := os.Stat(filepath)
+	if err == nil {
+		return true, nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	// Schrodinger cat, look for the error
+	return false, err
+}
+
+// IsChrooted returns false, if the current root belongs to host, true if this is a child root
+func (srm *SysrootManager) IsChrooted() (bool, error) {
+	hasHostSysroot, err := srm.fileExists(HostSysrootConfig)
+	if err != nil {
+		return false, fmt.Errorf("Unable to determine weather host sysroot config exists or not: %s", err.Error())
+	}
+	hasChildSysroot, err := srm.fileExists(ChildSysrootConfig)
+	if err != nil {
+		return false, fmt.Errorf("Unable to determine weather child sysroot config exists or not: %s", err.Error())
+	}
+
+	return hasChildSysroot && !hasHostSysroot, nil
 }
