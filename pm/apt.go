d5481f5427fefc1091649fa9413a008791945ac3
Bo Maryniuk <bo@maryniuk.net>
2022-10-05 14:17:09 +0200

    Add scaffold for the Apt/Dpkg implementation

--- pm/apt.go
+++ pm/apt.go
@@ -9,10 +9,34 @@ type AptPackageManager struct {
 	BasePackageManager
 }
 
+// Apt/dpkg package manager
 func NewAptPackageManager() *AptPackageManager {
 	pm := new(AptPackageManager)
 	pm.archFix = map[string]string{}
 	pm.env = map[string]string{}
 
 	return pm
+}
+
+// Call apt/dpkg
+func (pm *AptPackageManager) Call(args ...string) error {
+	return nil
+}
+
+// Name of the package manager
+func (pm *AptPackageManager) Name() string {
+	return "apt"
+}
+
+// SetSysroot to work with
+func (pm *AptPackageManager) SetSysroot(sysroot *sysmgr_sr.SysRoot) PackageManager {
+	pm.sysroot = sysroot
+	pm.sysroot.GetLogger().Debug("Apt environment:", pm.env)
+	return pm
+}
+
+// Setup package manager
+// This is used to pre-setup a package manager for a multiarch
+func (pm *AptPackageManager) Setup() error {
+	return nil
 }
