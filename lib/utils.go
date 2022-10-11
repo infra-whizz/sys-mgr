6d08d765f77bce72dbd9cdcd37412be6c0d29d20
Bo Maryniuk <bo@maryniuk.net>
2022-10-12 00:59:35 +0200

    Add streamed logging output (pipes through the wzlib logger)

---
+++ lib/utils.go
@@ -0,0 +1,26 @@
+package sysmgr_lib
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	wzlib_logger "github.com/infra-whizz/wzlib/logger"
+)
+
+type StdoutLogger struct {
+	wzlib_logger.WzLogger
+}
+
+func (sl *StdoutLogger) Write(p []byte) (n int, err error) {
+	sl.GetLogger().Info(strings.TrimSpace(string(p)))
+	return len(p), nil
+}
+
+func LoggedExec(cmd string, args ...string) error {
+	out := exec.Command(cmd, args...)
+	out.Stdin = os.Stdin
+	out.Stdout = &StdoutLogger{}
+	out.Stderr = os.Stderr
+	return out.Run()
+}
